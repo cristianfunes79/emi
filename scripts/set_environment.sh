ad112ed8d230b817b683078bfa910c139cb5f5b3
Cristian Funes <cfunes@lilasolutions.com.ar>
2021-12-05 07:35:52 -0300

    Se actualizo el script de seteo de environment.

--- scripts/set_environment.sh
+++ scripts/set_environment.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # this script must be sourced from repo root
-# pwd -> /home/user/emi
 # source scripts/set_environment.sh
 
+# Exports ------------------------------------------------------------------------------------
 export PROJECT_ROOT=$PWD
 export ESP8266_IDF=$PROJECT_ROOT/third_party/esp8266_idf/ESP8266_RTOS_SDK
 export ESP8266_TOOLCHAIN=$PROJECT_ROOT/third_party/esp8266_idf/toolchain/xtensa-lx106-elf/bin
@@ -11,9 +11,11 @@ export ESP32_IDF=$PROJECT_ROOT/third_party/esp32_idf/esp-idf
 export PATH="$ESP8266_TOOLCHAIN":$PATH
 export IDF_PATH=$ESP32_IDF
 
+# Includes ------------------------------------------------------------------------------------
 source $PROJECT_ROOT/scripts/bash_utils.sh
 source $ESP32_IDF/export.sh
 
+# Report --------------------------------------------------------------------------------------
 print "\n+------------------------------${COLOUR_BLUE}Project Environment:${COLOUR_NC}------------------------------+\n"
 print "${COLOUR_GREEN}PROJECT_ROOT:${COLOUR_NC} $PROJECT_ROOT"
 print "${COLOUR_GREEN}ESP8266_IDF:${COLOUR_NC}  $ESP8266_IDF"
